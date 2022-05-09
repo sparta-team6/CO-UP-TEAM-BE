1e7b23b5ff21de0b59ec0f48df975691c086c31e
kiwon kwon <funnykyeon@naver.com>
2022-05-09 14:45:52 +0900

    [fix] 프로젝트dto 최초작성

---
+++ src/main/java/hanghae/api/coupteambe/domain/dto/project/CreateProjectDto.java
@@ -0,0 +1,11 @@
+package hanghae.api.coupteambe.domain.dto.project;
+
+public class CreateProjectDto {
+
+    // 이미지주소
+    private String thumbnail;
+    //프로젝트제목
+    private String title;
+    //프로젝트개요
+    private String summary;
+}
---
+++ src/main/java/hanghae/api/coupteambe/domain/dto/project/ReqProjectInfoDto.java
@@ -0,0 +1,14 @@
+package hanghae.api.coupteambe.domain.dto.project;
+
+public class ReqProjectInfoDto {
+    //프로젝트번호
+    private String pjId;
+    // 이미지주소
+    private String thumbnail;
+    //프로젝트제목
+    private String title;
+    //프로젝트개요
+    private String summary;
+    //초대코드
+    private String inviteCode;
+}
