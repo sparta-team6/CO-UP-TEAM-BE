625dbaaa14539f34ffaa202949af9cd2ff2b4c61
kiwon kwon <funnykyeon@naver.com>
2022-05-09 11:28:02 +0900

    [init] 문서,폴더 dto 최초작성2

--- src/main/java/hanghae/api/coupteambe/domain/dto/document/FolderDto.java
+++ src/main/java/hanghae/api/coupteambe/domain/dto/document/FolderDto.java
@@ -1,5 +1,27 @@
 package hanghae.api.coupteambe.domain.dto.document;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FolderDto {
 
+    //폴더번호
+    private String dfId;
+    //폴더이름
+    private String title;
+    //배치순서
+    private String position;
+    //개설자
+    private String creator;
+    //생성시간
+    private String createdTime;
+    //수정시간
+    private String modifiedTime;
+    //삭제여부
+    private String delFlag;
+    //삭제여부
+    private String eliminator;
+
+    private List<DocumentDto> docs;
 }
