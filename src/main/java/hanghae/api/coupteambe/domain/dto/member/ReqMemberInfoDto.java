56e442bf51a64cd7c57e9d2284c7d46d23b58cdc
Jaeho Jung <iwogh3176@gmail.com>
2022-05-09 14:33:06 +0900

    [init] 회원관리dto 최초작성

--- src/main/java/hanghae/api/coupteambe/domain/dto/member/ReqMemberInfoDto.java
+++ src/main/java/hanghae/api/coupteambe/domain/dto/member/ReqMemberInfoDto.java
@@ -2,4 +2,19 @@ package hanghae.api.coupteambe.domain.dto.member;
 
 public class ReqMemberInfoDto {
 
+    // 멤버 번호
+    private String mbId;
+
+    // 프로필 이미지 URL
+    private String profileImage;
+
+    // 닉네임
+    private String nickname;
+
+    // 깃허브, 블로그 등 주소(URL)
+    private String url;
+
+    // 자기소개
+    private String aboutMe;
+
 }
