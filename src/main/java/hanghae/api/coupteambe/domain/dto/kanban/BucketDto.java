adbc369af7c247aaf421d837a488be73b54d387d
Jaeho Jung <iwogh3176@gmail.com>
2022-05-09 11:54:46 +0900

    [init] 버킷,카드 dto 최초작성2

--- src/main/java/hanghae/api/coupteambe/domain/dto/kanban/BucketDto.java
+++ src/main/java/hanghae/api/coupteambe/domain/dto/kanban/BucketDto.java
@@ -1,17 +1,13 @@
 package hanghae.api.coupteambe.domain.dto.kanban;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import javax.validation.constraints.Size;
 import java.util.List;
 
-@Setter
 @Getter
 public class BucketDto {
 
     // 버킷 제목
-    @Size(max = 20)
     private String title;
 
     // 해당 버킷 내 카드 정보
