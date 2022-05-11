1472ac250269f68c5eedebe8abfb0b4889ef4279
Seungmin <globalsh1@gmail.com>
2022-05-12 01:46:25 +0900

    [feat] 프로젝트 생성 #83

--- src/main/java/hanghae/api/coupteambe/service/ProjectService.java
+++ src/main/java/hanghae/api/coupteambe/service/ProjectService.java
@@ -1,8 +1,44 @@
 package hanghae.api.coupteambe.service;
 
+import hanghae.api.coupteambe.domain.dto.project.CreateProjectDto;
+import hanghae.api.coupteambe.domain.entity.member.Member;
+import hanghae.api.coupteambe.domain.entity.project.Project;
+import hanghae.api.coupteambe.domain.entity.project.ProjectMember;
+import hanghae.api.coupteambe.domain.repository.member.MemberRepository;
+import hanghae.api.coupteambe.domain.repository.project.ProjectMemberRepository;
+import hanghae.api.coupteambe.domain.repository.project.ProjectRepository;
+import hanghae.api.coupteambe.util.exception.ErrorCode;
+import hanghae.api.coupteambe.util.exception.RequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
 
+    private final ProjectRepository projectRepository;
+    private final ProjectMemberRepository projectMemberRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void create(CreateProjectDto createPJrojectDto) {
+
+        // 1. 프로젝트 생성 사용자 추출
+        String loginId = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<Member> optionalMember = memberRepository.findByLoginId(loginId);
+        Member member = optionalMember.orElseThrow(() -> new RequestException(ErrorCode.MEMBER_LOGINID_NOT_FOUND_404));
+
+        // 2. 프로젝트 생성
+        Project project = new Project(createPJrojectDto);
+
+        // 3. 프로젝트 연관관계 설정
+        ProjectMember projectMember = new ProjectMember(member, project);
+
+        // 4. 프로젝트 저장
+        projectMemberRepository.save(projectMember);
+    }
 }
