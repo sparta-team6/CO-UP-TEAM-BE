885739eaf452f8d27912a29f12f33aea4b5d10d6
kiwon kwon <funnykyeon@naver.com>
2022-05-17 11:57:22 +0900

    [feat] 폴더,문서 정렬로직2 구현

--- src/main/java/hanghae/api/coupteambe/service/member/MemberService.java
+++ src/main/java/hanghae/api/coupteambe/service/member/MemberService.java
@@ -44,6 +44,39 @@ public class MemberService {
     }
 
 
+//    /**
+//     * M4-8 멤버 검색(1인)
+//     */
+//    @Transactional
+//    public ResMemberInfoDto getOneMemberInfo(String loginId) {
+//
+//        // 1. 파라미터로 받은 loginId를 key로 멤버 정보를 조회한다
+//        Optional<Member> optionalMember = memberRepository.findByLoginId(loginId);
+//        Member member = optionalMember.orElseThrow(
+//                () -> new RequestException(ErrorCode.MEMBER_LOGINID_NOT_FOUND_404));
+//
+//        // 2. 새 멤버 객체를 생성하고 조회한 멤버로 초기화한다.
+//        // 3. 생성된 멤버 객체를 반환한다.
+//
+//        return new ResMemberInfoDto(member);
+//    }
+//    /// TODO: 2022/05/16 테스트해보자
+//
+//    /**
+//     * M4-9 본인 정보 조회
+//     */
+//    public ResMemberInfoDto getMyInfo() {
+//        // 1. 현재 로그인한 유저의 ID 조회
+//        String LoginId = SecurityContextHolder.getContext().getAuthentication().getName();
+//        // 2. 현재 멤버가 존재하지 않는 경우 예외처리
+//        Optional<Member> optionalMember = memberRepository.findByLoginId(LoginId);
+//        Member member = optionalMember.orElseThrow(() -> new RequestException(ErrorCode.MEMBER_LOGINID_NOT_FOUND_404));
+//
+//        // 3. 해당 멤버 조회 후 반환
+//        return new ResMemberInfoDto(member);
+//    }
+
+
     /**
      * M4-8 멤버 검색(1인)
      */
@@ -60,7 +93,7 @@ public class MemberService {
 
         return new ResMemberInfoDto(member);
     }
-    /// TODO: 2022/05/16 테스트해보자
+
 
     /**
      * M4-9 본인 정보 조회
