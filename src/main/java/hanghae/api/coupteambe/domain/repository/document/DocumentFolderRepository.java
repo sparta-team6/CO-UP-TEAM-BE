82b97152748dcc0c787868bdc7d0d26f4462f8c7
kiwon kwon <funnykyeon@naver.com>
2022-05-19 10:38:00 +0900

    [fix]폴더 position 카운팅 #157

--- src/main/java/hanghae/api/coupteambe/domain/repository/document/DocumentFolderRepository.java
+++ src/main/java/hanghae/api/coupteambe/domain/repository/document/DocumentFolderRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface DocumentFolderRepository extends JpaRepository<Folder, UUID>, DocumentFolderRepositoryCustom {
-
+    long countAllByproject_Id(UUID projectId);
 }
